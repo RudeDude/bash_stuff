d20333206fc603d53a791c89e64e163da1c945f2
Don J. Rude <djr7m@ccri.com>
2016-04-20 10:53:21 -0400

    Shell history stats.

---
+++ shellMost.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Looks at your bash history and tells you what you do the most
+
+echo Top commands:
+cat ~/.bash_history | cut -d " " -f 1 |sort |uniq -c |sort -grk 1 |head -20
+
+echo
+
+echo Top pipes:
+# history |sed 's/[0-9]\+ \+//' |grep '|' |cut -d "|" -f 2 |sed 's/ \+//' |cut -d " " -f 1 |sort |uniq -c |sort -grk 1 |head -20
+cat ~/.bash_history  |grep '|' |cut -d "|" -f 2 |sed 's/ \+//' |cut -d " " -f 1 |sort |uniq -c |sort -grk 1 |head -20
