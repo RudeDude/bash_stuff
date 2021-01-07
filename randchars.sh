89606ee2d0a8e23009eca689d1517afe48f07ed1
Don J. Rude <dr@counterflowai.com>
2021-01-07 11:47:40 -0500

    urandom, faster

--- randchars.sh
+++ randchars.sh
@@ -14,7 +14,7 @@ fi
 # Due to base64 inflation, maybe need to calculate something here
 # OUTLEN=$(( 4 * $COUNT / 3 ))
 
-dd if=/dev/random bs=1 count=${COUNT} 2>/dev/null |base64 -w 0 |sed 's/=//g'
+dd if=/dev/urandom bs=1 count=${COUNT} 2>/dev/null |base64 -w 0 |sed 's/=//g'
 ## If you have haveged it can be fast for getting random bytes
 # haveged -n ${COUNT} -f - 2>/dev/null |base64 -w 0 |sed 's/=//g'
 
