838501129c515cff71a1b4905a303f5e7a1bd10d
Don J. Rude <rudedude@gmail.com>
2022-11-17 16:22:48 -0500

    backup improvements. Looks for .ignore file

--- mybackup.sh
+++ mybackup.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 set -e
 
+## Where to put the compressed tarball
 DEST=/net/nas0/mnt/tank/backups/drude
-# Ensure destination exists and is writeable
-touch ${DEST}/last-backup-now
+## File to contain excluded file wildcards
+EXCL=".ignore"
+## Folders to backup
+DIRS="Desktop/ Documents/ .ssh/ Pictures/"
 
 # Ensure we are in the homedir
 cd ~
 
-echo initial tar file will be at least this big...
-du -shc Desktop/ Documents/ .ssh/ Pictures/
+for D in $DIRS
+do
+  T=`find $D -type f -name $EXCL`
+  for X in $T ; do
+    DN=`dirname $X`
+    echo $DN folder will ignore...
+    du -sh $DN
+  done
+done
+
+echo
+echo initial tar file will be at most this big \(minus ignores\)...
+du -shc $DIRS
 read -n 1 -r -p "Continue? "
 echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -17,14 +31,17 @@ then
   exit 1
 fi
 
+# Ensure destination exists and is writeable
+touch ${DEST}/last-backup-now
+
 echo Main folders...
-tar cf /tmp/backup.tar Desktop/ Documents/ .ssh/ Pictures/
+tar -c --exclude-ignore-recursive="$EXCL" -f /tmp/backup.tar $DIRS
 chmod go-rw /tmp/backup.tar
 du -h /tmp/backup.tar
 echo
 
 echo Find and add top level files...
-find . -maxdepth 1 -type f |xargs tar rvf /tmp/backup.tar
+find . -maxdepth 1 -type f |xargs -L 1 tar rvf /tmp/backup.tar
 du -h /tmp/backup.tar
 echo
 
@@ -35,8 +52,12 @@ echo
 
 mv /tmp/backup.tar.gz ./backup-`date -I`.tar.gz
 du -hc backup*.gz
-
+exit 0
 # scp backup-*.tar.gz ${DEST}
 # rm -f backup-*.tar.gz
+echo moving to destination...
 mv backup-*.tar.gz ${DEST}/
+
+# Update "now" timestamp
+touch ${DEST}/last-backup-now
 
