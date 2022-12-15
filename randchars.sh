1123a9233839bbd51e050ca0da72d60eec1e27a8
Don J. Rude <rudedude@gmail.com>
2022-12-15 11:29:15 -0500

    Add Cloudflare source. Add comments.

--- randchars.sh
+++ randchars.sh
@@ -14,9 +14,15 @@ fi
 # Due to base64 inflation, maybe need to calculate something here
 # OUTLEN=$(( 4 * $COUNT / 3 ))
 
+# Fetch random bytes from /dev/urandom a decent source
 dd if=/dev/urandom bs=1 count=${COUNT} 2>/dev/null |base64 -w 0 |sed 's/=//g'
-## If you have haveged it can be fast for getting random bytes
-# haveged -n ${COUNT} -f - 2>/dev/null |base64 -w 0 |sed 's/=//g'
+
+## If you have `haveged` it can be fast for getting random bytes
+haveged -n ${COUNT} -f - 2>/dev/null |base64 -w 0 |sed 's/=//g'
+
+# randomness from Cloudflare's edge servers. API info here: https://csprng.xyz/
+# Tends to have a trailing slash from the Base64 encoding
+#curl -s "https://csprng.xyz/v1/api?length=$COUNT" | jq --raw-output .Data | sed 's/=$//'
 
 echo
 
