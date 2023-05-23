b483a1d8663fde4a48aee0c9296a0b190dee18c7
Don J. Rude <rudedude@gmail.com>
2023-05-23 12:21:57 -0400

    Add note about fdupes software

--- find-dupe-file-names.sh
+++ find-dupe-file-names.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+echo "You may want to try and install the 'fdupes' program to find and remove more reliably."
 # Found at https://unix.stackexchange.com/questions/468440/find-all-files-with-the-same-name
 find -type f -print0 | awk -F/ 'BEGIN { RS="\0" } { n=$NF } k[n]==1 { print p[n]; } k[n] { print $0 } { p[n]=$0; k[n]++ }'
 
